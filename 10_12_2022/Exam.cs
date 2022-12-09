aa5cddca46a5294e9e3dd61642a527fb35c19da8
Dina <dinaobnaglela@yandex.ru>
2022-12-09 21:34:46 +0300

    Добавьте файлы проекта.

---
+++ 10_12_2022/Exam.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_12_2022
+{
+    class Exam
+    {
+        public string Name; 
+        private int rating; 
+        public int Rating //объявление свойства
+        {
+            get 
+            {
+                if (rating < 5)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return (rating + 1) / 2;
+                }
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    rating = 0;
+                }
+                else if (value > 10)
+                {
+                    rating = 10;
+                }
+                else
+                {
+                    rating = value;
+                }
+            }
+        }
+        public Exam(string name)  // конструктор
+        {
+            Name = name;
+        }
+    }
+}
---
+++ 10_12_2022/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_12_2022
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
